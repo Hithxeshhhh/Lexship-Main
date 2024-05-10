342f9ec4110d664fca419ce13da8995e5d802981
sarthak <bindalsarthak03@gmail.com>
2024-05-10 15:01:16 +0530

    test

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,8 +6,9 @@ const { convertController2 } = require('../controllers/convert2.controller');
 const { combinedController } = require('../controllers/combined.controller');
 const awbtopdfMiddleware = require('../middlewares/awbtopdf.middleware');
 const router = express.Router();
+
 router.get('/test',(req,res)=>{
-    res.send('Testing page')
+    res.json({message:'Testing page'})
 })
 router.post('/upload', uploadMiddleware, uploadController);
 router.post('/convert', convertController2);
