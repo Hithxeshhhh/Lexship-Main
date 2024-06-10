55b778f062f82334784f030d04f645e1ee11c759
sarthak <bindalsarthak03@gmail.com>
2024-06-10 11:32:03 +0530

    [REFACTOR] new mapping for customer details with ZOHO CRM 🔗

--- backend/controllers/createAccount.controller.js
+++ backend/controllers/createAccount.controller.js
@@ -18,7 +18,7 @@ const getCustomerDetails = async (customerId) => {
             'Content-Type': 'application/json',
         };
         const response = await axios.get(url, { headers });
-        return response.data[0];
+        return response.data;
     } catch (error) {
         throw new Error(`Failed to fetch customer details: ${error.message}`);
     }
@@ -55,61 +55,63 @@ const createZohoAccount = async (payload) => {
 
 //Constructing payload that is to be sent to create account
 const constructPayload = (customerId, zohoDealId, reqBodyData, customerDetails, dealDetails) => {
+    const addresses = Array.isArray(customerDetails[1]) ? customerDetails[1] : [];
+    const additionalInfo = Array.isArray(customerDetails[2]) ? customerDetails[2] : [];
+    
+    const billingAddressObject = addresses.find(item=>item.label === "Billing Address");
+    const shippingAddressObject = addresses.find(item=>item.label === "Registered Address");
+    const gstNumberObject = additionalInfo.find(item => item.key === "gst_number");
+    const iecCodeObject = additionalInfo.find(item => item.key === "iec_code");
+    const marketPlaceObject = additionalInfo.find(item=>item.key==="market_place_presence")
+    const productTypeObjhect = additionalInfo.find(item => item.key === "product_type_commodity")
     return {
         data: [
             {
-                Adcode: customerDetails.adcode,
-                Bank_Account_Number: customerDetails.bank_account,
+                Adcode: customerDetails[0].adcode,
+                Bank_Account_Number: customerDetails[0].bank_account,
                 Cust_Id: customerId,
                 Customer_ID: zohoDealId,
                 ...reqBodyData,
-                Mobile: customerDetails.mobile,
-                Email: customerDetails.email,
-                Account_Type: customerDetails.account_type,
-                IFSC_Code: customerDetails.ifsc_code,
-                Market_Place: customerDetails.Market_Place,
+                Mobile: customerDetails[0].mobile,
+                Email: customerDetails[0].email,
+                Account_Type: customerDetails[0].account_type,
+                IFSC_Code: customerDetails[0].ifsc_code,
+                Market_Place: marketPlaceObject ? marketPlaceObject.value : [],
                 Competitors: dealDetails.Competitors,
-                No_of_shipments: dealDetails.No_of_shipments1,
+                No_of_shipments: customerDetails[0].total_bookings,
                 Major_Destinations1: dealDetails.Major_Destinations1,
                 Expectations_of_the_customer_for_Services_Quot: dealDetails.Expectations_of_the_customer_for_Services_Quot,
                 Weight_Package: dealDetails.Weight_Package,
-                Type_of_business: dealDetails.Type_of_business,
+                Type_of_business: customerDetails[0].type_of_business,
                 Locked__s: dealDetails.Locked__s,
                 Tag: dealDetails.Tag,
+                Category : productTypeObjhect ? productTypeObjhect.value : "",
                 Prospect_Name: dealDetails.Deal_Name,
                 Prospect_Stage: dealDetails.Stage,
-                Account: customerDetails.Account||"",
-                Account_Status: customerDetails.Account_Status||"",
-                Account_Type1: customerDetails.Account_Type1||"",
-                Adcode_Certificate: customerDetails.Adcode_Certificate||"",
-                Address_Proof: customerDetails.Address_Proof||"",
-                Billing_City: customerDetails.Billing_City||"",
-                Billing_Code: customerDetails.Billing_Code||"",
-                Billing_Country: customerDetails.Billing_Country||"",
-                Billing_State: customerDetails.Billing_State||"",
-                Billing_State1: customerDetails.Billing_State1||"",
-                Billing_Street: customerDetails.Billing_Street||"",
-                Account_Number: customerDetails.Account_Number||"",
-                GST_Certificate: customerDetails.GST_Certificate||"",
-                GST_Number: customerDetails.GST_Number||"",
-                Identity_Proof: customerDetails.Identity_Proof||"",
-                IEC_Certificate: customerDetails.IEC_Certificate||"",
-                IEC_Code: customerDetails.IEC_Code||"",
-                Industry: customerDetails.Industry||"",
-                LUT_Certificate: customerDetails.LUT_Certificate||"",
-                LUT_Expiration_Date: customerDetails.LUT_Expiration_Date||"",
-                Phone: customerDetails.Phone||"",
-                Sales_Person_Name: customerDetails.Sales_Person_Name||"",
-                Seller: customerDetails.Seller||"",
-                Seller_ID: customerDetails.Seller_ID||"",
-                Shipping_City: customerDetails.Shipping_City||"",
-                Shipping_Code: customerDetails.Shipping_Code|| "",
-                Shipping_Country: customerDetails.Shipping_Country||"",
-                Shipping_State: customerDetails.Shipping_State||"",
-                Shipping_State1: customerDetails.Shipping_State1||"",
-                Shipping_Street: customerDetails.Shipping_Street||"",
-                Upload_Address_Proof: customerDetails.Upload_Address_Proof||"",
-                Upload_Identity_Proof: customerDetails.Upload_Identity_Proof||"",
+                Account: customerDetails[0].account||"",
+                Account_Status: customerDetails[0].status||"",
+                Account_Type1: customerDetails[0].acc_type||"",
+                Billing_City: billingAddressObject ? billingAddressObject.city : "",
+                Billing_Code: billingAddressObject ? billingAddressObject.postal_code :"",
+                Billing_Country: billingAddressObject ? billingAddressObject.country_code : "",
+                Billing_State: billingAddressObject ? billingAddressObject.state : "",
+                Billing_Street: billingAddressObject ? billingAddressObject.street : "",
+                Account_Number: customerDetails[0].account||"",
+                GST_Certificate: null,
+                GST_Number: gstNumberObject ? gstNumberObject.value : "",
+                IEC_Certificate: null,
+                IEC_Code: iecCodeObject ? iecCodeObject.value : "",
+                LUT_Certificate: null,
+                LUT_Expiration_Date: customerDetails[0].LUT_Expiration_Date||"",
+                Sales_Person_Name: customerDetails[0].company_name,
+                Seller_ID: "LEX",
+                Shipping_City: shippingAddressObject ? shippingAddressObject.city : "",
+                Shipping_Code: shippingAddressObject ? shippingAddressObject.postal_code : "",
+                Shipping_Country: shippingAddressObject ? shippingAddressObject.country_code : "",
+                Shipping_State: shippingAddressObject ? shippingAddressObject.state : "",
+                Shipping_Street: shippingAddressObject ? shippingAddressObject.street : "",
+                Upload_Address_Proof: null,
+                Upload_Identity_Proof: null,
             }
         ]
     };
@@ -124,7 +126,7 @@ exports.createAccountController = async (req, res) => {
             return res.status(400).json({ error: 'Customer_id is required' });
         }
         const customerDetails = await getCustomerDetails(customerId);
-        const zohoDealId = customerDetails.Zoho_Deal_ID;
+        const zohoDealId = customerDetails[0].Zoho_Deal_ID;
         const dealDetails = await getZohoDealDetails(zohoDealId);
         const payload = constructPayload(customerId, zohoDealId, reqBodyData, customerDetails, dealDetails);
         console.log("Payload being sent to Zoho Accounts API:", JSON.stringify(payload, null, 2));
