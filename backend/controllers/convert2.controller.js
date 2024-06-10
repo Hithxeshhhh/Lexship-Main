70914df9e49f94e86631b0d352b901ad113b7730
sarthak <bindalsarthak03@gmail.com>
2024-06-10 17:07:20 +0530

    [REFACTOR] log statements added for error detection 🔎

--- backend/controllers/convert2.controller.js
+++ backend/controllers/convert2.controller.js
@@ -1,15 +1,18 @@
-
 const archiver = require('archiver');
 const { PDFNet } = require('@pdftron/pdfnet-node');
 const fs = require('fs');
 const path = require('path');
 const im = require('imagemagick');
 require('dotenv').config();
+const { exec } = require('child_process');
 
 PDFNet.initialize(process.env.API_LICENSE_KEY);
 
+
+
 async function compresstozip(folderPath) {
     return new Promise((resolve, reject) => {
+        console.log(`Compressing files in ${folderPath} to ZIP...`);
         const output = fs.createWriteStream('compressed.zip');
         const archive = archiver('zip', {
             zlib: { level: 9 }
@@ -21,6 +24,7 @@ async function compresstozip(folderPath) {
         });
 
         archive.on('error', function (err) {
+            console.error('Error creating ZIP archive:', err);
             reject(err);
         });
 
@@ -41,6 +45,7 @@ async function combineTiffs() {
     try {
         const uploadsFolder = path.join(__dirname, '../uploads/');
         const outputFolder = path.join(__dirname, '../convertedTif/');
+        console.log('Combining TIFF files...');
 
         const files = await fs.promises.readdir(uploadsFolder);
         const pdfFiles = files.filter(file => path.extname(file).toLowerCase() === '.pdf');
@@ -81,7 +86,6 @@ async function combineTiffs() {
                 });
             });
         });
-
         await Promise.all(combineTasks);
         console.log('All PDFs converted and TIFF files combined.');
 
@@ -90,70 +94,86 @@ async function combineTiffs() {
     }
 }
 
+
+
+
 exports.convertController2 = async (req, res) => {
     const io = req.app.get('socketio');
     try {
+
         const { successfulDownloads, failedDownloads } = req;
-        if(JSON.stringify(successfulDownloads).length!==2){
+        if (JSON.stringify(successfulDownloads).length !== 2) {
             const inputFolder = path.join(__dirname, '../uploads/');
             const outputFolder = path.join(__dirname, '../convertedTif/');
             
             const pdfFiles = fs.readdirSync(inputFolder).filter(file => file.endsWith('.pdf'));
             let completedTasks = pdfFiles.length;
-        const totalTasks = pdfFiles.length*2; // Number of steps in the process
+            const totalTasks = pdfFiles.length * 2; // Number of steps in the process
 
-        for (const pdfFile of pdfFiles) {
-            // Convert code
-            const pdfFilePath = path.join(inputFolder, pdfFile);
-            const outputFileName = path.parse(pdfFile).name;
-            const outputFolderPath = path.join(outputFolder, outputFileName);
+            console.log('Starting PDF to TIFF conversion process...');
+            console.log(`PDF Files: ${JSON.stringify(pdfFiles)}`);
 
-            if (!fs.existsSync(outputFolderPath)) {
-                fs.mkdirSync(outputFolderPath);
+            for (const pdfFile of pdfFiles) {
+                // Convert code
+                const pdfFilePath = path.join(inputFolder, pdfFile);
+                const outputFileName = path.parse(pdfFile).name;
+                const outputFolderPath = path.join(outputFolder, outputFileName);
+
+                if (!fs.existsSync(outputFolderPath)) {
+                    fs.mkdirSync(outputFolderPath);
+                }
+
+                console.log(`Converting PDF ${pdfFile} to TIFF...`);
+                const doc = await PDFNet.PDFDoc.createFromFilePath(pdfFilePath);
+                const pdfDraw = await PDFNet.PDFDraw.create();
+                pdfDraw.setDPI(200);
+                const pageCount = await doc.getPageCount();
+                for (let pageNum = 1; pageNum <= pageCount; pageNum++) {
+                    const page = await doc.getPage(pageNum);
+                    await pdfDraw.export(page, `${outputFolderPath}/page_${pageNum}.tif`, 'TIFF');
+                }
+                doc.destroy();
+                console.log(`PDF ${pdfFile} converted to TIFF successfully.`);
+                completedTasks++;
+                io.emit('progress', { completed: completedTasks, total: totalTasks });
             }
-            const doc = await PDFNet.PDFDoc.createFromFilePath(pdfFilePath);
-            const pdfDraw = await PDFNet.PDFDraw.create();
-            pdfDraw.setDPI(200);
-            const pageCount = await doc.getPageCount();
-            for (let pageNum = 1; pageNum <= pageCount; pageNum++) {
-                const page = await doc.getPage(pageNum);
-                await pdfDraw.export(page, `${outputFolderPath}/page_${pageNum}.tif`, 'TIFF');
-            }
-             doc.destroy();
-             completedTasks++;
-             io.emit('progress', { completed: completedTasks, total: totalTasks });
+
+            await combineTiffs();
+            console.log('Compressing TIFF files to ZIP...');
+            const zipFilePath = await compresstozip(outputFolder); // Generate the ZIP file
+            const zipFile = fs.readFileSync(zipFilePath);
+            
+            fs.unlinkSync(zipFilePath);
+            console.log(`ZIP file ${zipFilePath} created and deleted after reading.`);
+
+            pdfFiles.forEach(pdfFile => {
+                const pdfFilePath = path.join(inputFolder, pdfFile);
+                fs.unlinkSync(pdfFilePath);
+                console.log(`PDF file ${pdfFilePath} deleted.`);
+            });
+
+            const tifFiles = fs.readdirSync(outputFolder).filter(file => file.endsWith('.tif'));
+            tifFiles.forEach(tifFile => {
+                const tifFilePath = path.join(outputFolder, tifFile);
+                fs.unlinkSync(tifFilePath);
+                console.log(`TIFF file ${tifFilePath} deleted.`);
+            });
+            res.setHeader('Content-Type', 'application/zip');
+            res.setHeader('Content-Disposition', 'attachment; filename=converted_files.zip');
+            res.setHeader('successful', JSON.stringify(successfulDownloads));
+            res.setHeader('failed', JSON.stringify(failedDownloads));
+            res.status(200).send(zipFile);
+            console.log('PDF to TIFF conversion process completed and ZIP file sent.');
+        } else {
+            res.setHeader('successful', JSON.stringify(successfulDownloads));
+            res.setHeader('failed', JSON.stringify(failedDownloads));
+            res.status(200).send('');
+            console.log('No successful downloads, returning empty response.');
         }
-        await combineTiffs();
-        const zipFilePath = await compresstozip(outputFolder); // Generate the ZIP file
-        const zipFile = fs.readFileSync(zipFilePath);
-        
-        fs.unlinkSync(zipFilePath);
-        
-        pdfFiles.forEach(pdfFile => {
-            const pdfFilePath = path.join(inputFolder, pdfFile);
-            fs.unlinkSync(pdfFilePath);
-        });
-
-        const tifFiles = fs.readdirSync(outputFolder).filter(file => file.endsWith('.tif'));
-        tifFiles.forEach(tifFile => {
-            const tifFilePath = path.join(outputFolder, tifFile);
-            fs.unlinkSync(tifFilePath);
-        });
-        res.setHeader('Content-Type', 'application/zip');
-        res.setHeader('Content-Disposition', 'attachment; filename=converted_files.zip');
-        res.setHeader('successful',JSON.stringify(successfulDownloads))
-        res.setHeader('failed',JSON.stringify(failedDownloads))
-        res.status(200).send(zipFile);
-    }else{
-        res.setHeader('successful',JSON.stringify(successfulDownloads))
-        res.setHeader('failed',JSON.stringify(failedDownloads))
-        res.status(200).send('')
+    } catch (error) {
+        console.error('Error converting PDFs to TIFF:', error);
+        res.status(500).send('Error converting PDFs to TIFF.');
     }
-    
-} catch (error) {
-    console.error('Error converting PDFs to TIFF:', error);
-    res.status(500).send('Error converting PDFs to TIFF.');
-}
 };
 
 PDFNet.shutdown(); 
