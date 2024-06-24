216bd5d1b00d8daf04a60cb601bbb9734cbdb53a
sarthak <bindalsarthak03@gmail.com>
2024-06-24 13:13:33 +0530

    [DELETE] unnecessary files discarded 🗑️

--- backend/controllers/upload.controller.js
+++
@@ -1,8 +0,0 @@
-exports.uploadController = async (req, res) => {
-    const uploadedFiles = req.files; 
-    if (!uploadedFiles || uploadedFiles.length === 0) {
-        res.status(400).json({ error: 'No files uploaded' });
-        return;
-    }
-    res.status(200).json({ uploadedFiles });
-};
