894bc918c290f23beac0036252d85528e9dd388b
sarthak <bindalsarthak03@gmail.com>
2024-06-24 13:14:15 +0530

    [CHORE] removed unnecessary lines 🗑️

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const uploadMiddleware = require('../middlewares/upload.middleware')(); 
-const { uploadController } = require('../controllers/upload.controller');
-const { convertController } = require('../controllers/convert.controller');
 const { convertController2 } = require('../controllers/convert2.controller');
-const { combinedController } = require('../controllers/combined.controller');
 const awbtopdfMiddleware = require('../middlewares/awbtopdf.middleware');
 const { createLeadController } = require('../controllers/createLead.controller');
 const { createDealController } = require('../controllers/createDeal.controller');
@@ -22,8 +18,6 @@ router.get('/test',(req,res)=>{
 })
 
 //module routes
-router.post('/upload', uploadMiddleware, uploadController);
-router.post('/convert', convertController2);
 router.post('/upload-convert',awbtopdfMiddleware,convertController2);
 
 //Zoho integration routes
