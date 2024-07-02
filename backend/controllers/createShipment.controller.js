7d89d137407869f3e07e3fa2ec28923ba7fbc4b4
sarthak <bindalsarthak03@gmail.com>
2024-07-02 12:58:28 +0530

    [CHORE] passing data as hardcoded json object (tentative)

--- backend/controllers/createShipment.controller.js
+++ backend/controllers/createShipment.controller.js
@@ -14,13 +14,35 @@ exports.createShipmentController = async (req, res) => {
             'Content-Type': 'application/json'
         }
         const payload = {
-            data:[{
-                ...req.body,
-            }]
+            data:[
+                {
+                    "Account_Status":"active",
+                    "Customer_Name":"444217000001307032", //account_id
+                    "Billed_Wt":"0.60",
+                    "Name":"Dummy Shipment 1101", //account_id
+                    "Billed_Weight":"0.6",
+                    "Cust_ID_s":"234",
+                    "Country":"GB",
+                    "Created_By":"444217000001307032",//account_id
+                    "Customer_ID":"444217000001307032",//account_id
+                    "Customer_Types":"LEX",
+                    "Customer_Wt":"0.54",
+                    "Email":"abc@gmail.com",
+                    "Hub_Wt":"0.60",
+                    "IGST":"200",
+                    "INV_TOTAL":"800",
+                    "Product_Type":"Gift",
+                    "Proforma_Value":"800",
+                    "Prospect_Name":"444217000001319012",//prospect_id
+                    "TOTAL_SALE_AMT":"800",
+                    "Top_Countries":"GB" 
+                }
+            ]
         }
         const response = await axios.post(ZOHO_SHIPMENTS_API,payload,{headers});
         res.status(200).json(response.data);
     } catch (error) {
-        res.status(500).send(`Failed to create Zoho deal : ${error}`);
+        console.error('Error creating Zoho shipment:', error.response ? JSON.stringify(error.response.data, null, 2) : error.message);
+        res.status(500).send(`Failed to create Zoho shipment: ${error.response ? JSON.stringify(error.response.data, null, 2) : error.message}`);
     }
 }
