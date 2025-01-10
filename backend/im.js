e05a50dbd433baf2b0d2b20f65733c445576e1b7
Hithesh <hiteshberg907@gmail.com>
2025-01-10 16:47:01 +0530

    Deleted output.pdf

--- backend/im.js
+++
@@ -1,9 +0,0 @@
-const imagemagick = require('imagemagick');
-
-imagemagick.convert(['input.tif', '-compress', 'LZW', 'output.tif'], (err, stdout) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('Conversion successful:', stdout);
-  }
-});
